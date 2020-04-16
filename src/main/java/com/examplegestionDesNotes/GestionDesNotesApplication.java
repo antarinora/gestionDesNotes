d015f2b2cb8409c52bba24196290306cc7d2d781
HP <HP@HP-PC>
2020-04-17 00:18:03 +0100

    first commit

---
+++ src/main/java/com/examplegestionDesNotes/GestionDesNotesApplication.java
@@ -0,0 +1,13 @@
+package com.examplegestionDesNotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDesNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDesNotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/examplegestionDesNotes/GestionDesNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examplegestionDesNotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDesNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
