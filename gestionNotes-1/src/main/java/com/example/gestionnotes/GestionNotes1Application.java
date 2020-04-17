0528ef8da77637e152ba0550fd76d9276f375359
Admin <Admin@Admin-PC>
2020-04-17 03:40:13 +0100

    commit projet

---
+++ gestionNotes-1/src/main/java/com/example/gestionnotes/GestionNotes1Application.java
@@ -0,0 +1,13 @@
+package com.example.gestionnotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionNotes1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionNotes1Application.class, args);
+	}
+
+}
---
+++ gestionNotes-1/src/test/java/com/example/gestionnotes/GestionNotes1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestionnotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionNotes1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
